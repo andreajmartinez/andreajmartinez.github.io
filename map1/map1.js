080474738b8ce336e8f33edc751d4097c23e9f43
Andrea Martinez <andreamartinez@Andreas-MacBook-Air-2.local>
2020-04-16 01:59:12 -0500

    map

--- map1/map1.js
+++ map1/map1.js
@@ -1,17 +1,17 @@
-var texas = L.map('Texas').setView([31.69, -100], 7)
-L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png').addTo(texas)
-var houston = L.marker([29.801951, -95.355999]).addTo(texas)
+var mymap = L.map('mapid').setView([31.69, -100], 7)
+L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png').addTo(mymap)
+var houston = L.marker([29.801951, -95.355999]).addTo(mymap)
 var southtexas = L.polygon([
   [36.491, -103.046],
   [33.504, -94.125],
   [29.916, -93.510]
-]).addTo(texas)
+]).addTo(mymap)
 houston.bindPopup('City of Houston.')
 southtexas.bindPopup('Area of Southern Texas.')
 var latlngs = [
   [36.491, -103.046],
   [25.996, -97.377]
 ]
-var polyline = L.polyline([latlngs, { color: 'red' }]).addTo(texas)
-texas.fitBounds(polyline.getBounds())
+var polyline = L.polyline([latlngs, { color: 'red' }]).addTo(mymap)
+mymap.fitBounds(polyline.getBounds())
 polyline.bindPopup('Line Across Texas')
