bfdfa3b50a14fd9348b1861e4e0b4b591f482fed
Andrea Martinez <andreamartinez@Andreas-MacBook-Air-2.local>
2020-04-21 12:18:28 -0500

    final changes

--- map3/map3.js
+++ map3/map3.js
@@ -1,36 +1,36 @@
 var mymap = L.map('mapid').setView([32.18, -99.14], 4)
-var statesLayerObject = L.layerGroup().addTo(mymap)
-var grayBasemapObject = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}').addTo(mymap)
+var statesLayer = L.layerGroup().addTo(mymap)
+var grayBasemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}').addTo(mymap)
+var streetsBasemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}').addTo(mymap)
 var demographics = 'https://geog4046.github.io/assignment-resources/data/us_state_demographics_ESRI_2010A.geojson'
-var streetsBasemapObject = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}').addTo(mymap)
-var basemapsObject = {
-  'Streets': streetsBasemapObject,
-  'Gray canvas': grayBasemapObject
-}
+
 jQuery.getJSON(demographics, function (data) {
-  var colors = function (feature) {
-    var femalepop = feature.properties.SQMI
-    var color1 = 'olive'
-    if (area < 70000) { color1 = 'green' }
-    return {
-      color: color1
-      weight: 1,
-      fillOpacity: 0.5
-    }
-  }
-  var renameThisGeojsonOptionsObject = {
-    style: renameThisStyleFunction,
-    onEachFeature: renameThisOnEachFeatureFunction
-  }
-  L.geoJSON(data, renameThisGeojsonOptionsObject).addTo(mymap)
+  L.geoJSON(data, {
+    style: compareArea,
+    onEachFeature: areaResults
+  }).addTo(mymap)
 })
-var renameThisOnEachFeatureFunction = function (feature, layer) {
-  var name = feature.properties.STATE_NAME
-  var area = feature.properties.SQMI
-  layer.bindPopup('Area ' + name + ': ' + area + '<br>National average: 71907')
-  statesLayerObject.addLayer(layer)
+var compareArea = function (feature) {
+  var stateArea = feature.properties.SQMI
+  var stateColor = '#FFDAB9'
+  if (stateArea > 43434.42) { stateColor = '#DA0DDD' }
+  return {
+    color: stateColor,
+    weight: 1,
+    fillOpacity: 0.5
+  }
 }
-var layersObject = {
-  'Area': statesLayerObject
+var areaResults = function (feature, layer) {
+  var stateName = feature.properties.STATE_NAME
+  var stateArea = feature.properties.SQMI
+  layer.bindPopup('The area of ' + stateName + ' is ' + stateArea + '<br>The national mean area is: 43434.42')
+  statesLayer.addLayer(layer)
 }
-L.control.layers(basemapsObject, layersObject).addTo(mymap)
+var basemaps = {
+  'Streets': streetsBasemap,
+  'Grey Canvas': grayBasemap
+}
+var layers ={
+  'Area of each state': statesLayer
+}
+L.control.layers(basemaps, layers).addto(mymap)
